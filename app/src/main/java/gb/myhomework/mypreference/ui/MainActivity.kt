284717a5f45a7a0c1ba65e83e78d049ec2efb47d
AlexEvdokimovSpb <3416216@gmail.com>
2021-02-03 21:55:43 +0300

    add Navigation menu добавил меню новигации, к сожалению не получилось прибайндить элементы из дочерних лойаутов. Элементы при байдинге видны, но не используются.

--- app/src/main/java/gb/myhomework/mypreference/ui/MainActivity.kt
+++ app/src/main/java/gb/myhomework/mypreference/ui/MainActivity.kt
@@ -7,25 +7,37 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.firebase.ui.auth.AuthUI
+import com.google.android.material.navigation.NavigationView
 import gb.myhomework.mypreference.Constants
 import gb.myhomework.mypreference.R
 import gb.myhomework.mypreference.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), LogoutDialog.LogoutListener {
+class MainActivity : AppCompatActivity(), LogoutDialog.LogoutListener,
+    NavigationView.OnNavigationItemSelectedListener {
 
     val TAG = "HW " + MainActivity::class.java.simpleName
-    lateinit var ui: ActivityMainBinding
+
+    val ui: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ui = ActivityMainBinding.inflate(layoutInflater)
         setContentView(ui.root)
-        setSupportActionBar(ui.toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        ui.button.setOnClickListener {
+        val toolbar: Toolbar = initToolbar()
+        initDrawer(toolbar)
+
+        val button = ui.root.findViewById<View>(R.id.button) as Button
+        button.setOnClickListener {
             val intent = Intent(this, HistoryActivity::class.java)
             startActivity(intent)
             if (Constants.DEBUG) {
@@ -38,14 +50,14 @@ class MainActivity : AppCompatActivity(), LogoutDialog.LogoutListener {
         MenuInflater(this).inflate(R.menu.menu_main, menu).let { true }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean =
-        when(item.itemId) {
+        when (item.itemId) {
             R.id.logout -> showLogoutDialog().let { true }
             else -> false
         }
 
     private fun showLogoutDialog() {
-        supportFragmentManager.findFragmentByTag(LogoutDialog.TAG) ?:
-        LogoutDialog.createInstance().show(supportFragmentManager, LogoutDialog.TAG)
+        supportFragmentManager.findFragmentByTag(LogoutDialog.TAG) ?: LogoutDialog.createInstance()
+            .show(supportFragmentManager, LogoutDialog.TAG)
     }
 
     override fun onLogout() {
@@ -59,5 +71,55 @@ class MainActivity : AppCompatActivity(), LogoutDialog.LogoutListener {
 
     companion object {
         fun getStartIntent(context: Context) = Intent(context, MainActivity::class.java)
+    }
+
+    private fun initDrawer(toolbar: Toolbar) {
+        val drawer = ui.drawerLayout
+        val navigationView = ui.navView
+        val toggle = ActionBarDrawerToggle(
+            this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+        navigationView.setNavigationItemSelectedListener(this)
+    }
+
+    private fun initToolbar(): Toolbar {
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        return toolbar
+    }
+
+    @Override
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        when (id) {
+            R.id.nav_reserved_1 -> {
+                // TO DO
+            }
+            R.id.nav_reserved_2 -> {
+                // TO DO
+            }
+            R.id.nav_reserved_3 -> {
+                // TO DO
+            }
+            R.id.nav_history -> {
+                val intent = Intent(this, HistoryActivity::class.java)
+                startActivity(intent)
+            }
+            // TO DO
+        }
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        val drawer = ui.drawerLayout
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
